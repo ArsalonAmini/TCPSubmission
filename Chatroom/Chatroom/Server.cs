5202b0f633990b8ad626e9f88539ff10f0acc840
ArsalonAmini <aminihajibasa@gmail.com>
2016-09-06 07:52:04 -0500

    Inital Commit

---
+++ Chatroom/Chatroom/Server.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Collections;
+using System.Net;
+
+
+namespace Chatroom
+{
+    class Server
+    {
+        //create a TCP listener
+        //accept a TCP client
+        //create a network stream
+        //read from network stream 
+        IPAddress localAddress;
+        TcpListener chatServer;
+        TcpClient clientSocket;
+        NetworkStream networkStream;
+        string dataFromClient;
+        public byte[] bytesFrom;
+        Hashtable clientsList;
+        Int32 port = 10000;
+
+        public Server()
+        {
+            localAddress = IPAddress.Parse("10.2.20.30");
+            chatServer = new TcpListener (localAddress, port);
+            clientsList = new Hashtable();
+        }
+        public void ListenForRequests()
+        {
+            chatServer.Start(); //start listening for clients 
+            Console.WriteLine("Waiting for a connection...");
+        }
+
+        public void AcceptIncomingRequests()
+        {
+            clientSocket = chatServer.AcceptTcpClient(); //creating a member variable, type TcPclient to store accepted client                     
+            Console.WriteLine("Connected!");
+        }
+
+        public void ReceiveMessages()
+        {
+            networkStream = clientSocket.GetStream();
+            networkStream.Read(bytesFrom, 0, bytesFrom.Length); //read client stream
+            dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);  //encode message
+            Console.WriteLine("Received: {0}", dataFromClient);
+        }
+
+        public void SendBroadCastMessages()
+        {
+            foreach(DictionaryEntry Item in clientsList)
+            {
+                //
+            }
+        }
+
+        public void AddClientsToList()
+        {
+            clientsList.Add(dataFromClient, clientSocket);
+        }
+    }
+}
---
+++ Chatroom/Client/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Windows;
+
+
+namespace Client
+{
+    class Client
+    {
+        TcpClient clientSocket;
+        NetworkStream stream;
+        Int32 port;
+        byte[] outData;
+        byte[] inStream;
+        string message;
+        public Client()
+        {
+            port = 10000;
+            
+        }
+        public void ConnectToServer()
+        {
+            clientSocket = new TcpClient("10.2.20.30", port);
+            stream = clientSocket.GetStream();
+        }
+
+        public void SendMessage()
+        {
+            Console.WriteLine("Enter Message:");
+            message = Console.ReadLine();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+        }
+        public void GetMessage()
+        {
+            inStream = new Byte[256];
+            Int32 bytes = stream.Read(inStream, 0, inStream.Length);
+
+        }
+
+    }
+}
